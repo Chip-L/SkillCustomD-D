f520da5d2f2ccfb41ba5ab151cc1748bdd353177
Chip Long <chip_long@yahoo.com>
2018-06-09 22:42:37 -0600

    test variable 5

--- resources/js/board.js
+++ resources/js/board.js
@@ -3,7 +3,19 @@
 var Board = window.Board || {};
 
 Board.Screen = (function($) {
-  var Screen = function (className) {
+  var Screen = function () {
+
+    this.init = function () {
+      $header = $('.header');
+      $footer = $('.footer');
+      $mainBody = $('.main_body');
+
+      $mainBody.attr('height', $header.attr('height') + $footer.attr('height'));
+    }
+
+  }
+
+  var setScreen = function (className) {
     return $(className).attr("height");
   }
 }) (jQuery);
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,4 @@
 $(document).ready( function () {
   board = new Board.Screen;
+  board.init();
 });
